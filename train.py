d01cf2ab957f2ffca8c7b18f2790b90bf5520079
SongOfTheValkyrie <42936499+SongOfTheValkyrie@users.noreply.github.com>
2022-01-25 23:36:59 +0100

    huge update still buggy

--- train.py
+++ train.py
@@ -1,17 +1,12 @@
-from argparse import ArgumentParser
+from keras.preprocessing.image import ImageDataGenerator
 import os
 import numpy as np
 import cv2
-from numpy import empty
-from sklearn.datasets import load_iris
 import tensorflow.compat.v1 as tf
-from numpy import expand_dims
-from keras.preprocessing.image import ImageDataGenerator
-from mobilenetv3_factory import build_mobilenetv3
 from datasets import build_dataset
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import load_img
+from mobilenetv3 import MobileNetV3
 
 dataset_path="C:/Users/Jasmin/Documents/RunesApp/Runes"
 tf.disable_v2_behavior() 
@@ -33,35 +28,24 @@ def load_dataset():
             folder_list=image[1]
         train_images_x.append(image[2]) 
         for i in train_images_x[count]:
-            z=(str(dataset_path)+"/"+str(folder_list[count-1])+"/"+str(i))
-            x.append(cv2.imread(z,cv2.IMREAD_UNCHANGED))
-            y.append(count-1)
+            path=(str(dataset_path)+"/"+str(folder_list[count-1])+"/"+str(i))
+            img = cv2.imread(path ,cv2.IMREAD_UNCHANGED)
+            if img is None:
+                print("there is broken data:" ,path)
+            else:
+                dim = (224,224)
+                # resize image
+                img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+                img = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+                img = np.expand_dims(img, axis=0)
+                img = img/255
+                x.append(img)
+                y.append(count-1)
+            
         count+=1
 
-    print("Succesfully load Dataset!")
-    
-    
-def data_preprocessing():
-   
-    count=0
-    y_res=[]
-    x_res=[]
-    height = 224
-    width = 224
-    dim = (width, height)
-    for img in x:
-        res_img = []
-        if img is None:
-            print("there is broken data at index ",count)
-        else:
-            for i in range(len(img)-1):
-                res = cv2.resize(img[i], dim, interpolation=cv2.INTER_LINEAR)
-                res_img.append(res)
-            x_res.append(np.array(res_img))
-            y_res.append(y[count])
-        count+=1
-    print("Preprocessing terminated succesfully!")
-    return x_res, y_res
+    print("Succesfully load and prepared Dataset!")
+    return x,y
     
     
 
@@ -69,11 +53,10 @@ def build_dataset():
     train_X, test_X, train_y, test_y = train_test_split(x, y, 
                                                     train_size=0.6,
                                                     test_size=0.4)
-    test_X,test_y, eval_x, eval_y = train_test_split(x, y, 
-                                                    train_size=0.5,
-                                                    test_size=0.5)
+   
 
-    return train_X,test_X,train_y,test_y, eval_x,eval_y
+    return train_X,test_X,train_y,test_y
+
 
 _available_optimizers = {
     "rmsprop": tf.keras.optimizers.RMSprop(),
@@ -81,12 +64,7 @@ _available_optimizers = {
     "sgd": tf.keras.optimizers.SGD(),
     }
 
-model = build_mobilenetv3(
-    "large",
-    input_shape=(224, 224, 3),
-    num_classes=1001,
-    width_multiplier=1.0,
-)
+model = MobileNetV3((224,224,3),4).build()
 
 
 model.compile(
@@ -95,28 +73,14 @@ model.compile(
         metrics=["accuracy"],
     )
 
+x,y=load_dataset()
 
-#model.save_weights(f"mobilenetv3_{args.model_type}_{args.dataset}_{epochs}.h5")
+x_train,x_test, x_eval,y_eval=build_dataset()
 
-
-load_dataset()
-x,y = data_preprocessing()
-
-x_train,x_test,y_train,y_test, x_eval,y_eval=build_dataset()
-
-img_rows=np.array(x_train[0]).shape[0]
-img_cols=np.array(x_test[0]).shape[1]
-
-X_train=np.array(x_train).reshape(np.array(x_train).shape[0],img_rows,img_cols,1)
-
-X_test=np.array(x_test).reshape(np.array(x_test).shape[0],img_rows,img_cols,1)
-
-
-Input_shape=(img_rows,img_cols,1)
 
 model.fit(
         x_train,
-        y_train,
+        x_test,
         batch_size=10,
         epochs=2,
         # We pass some validation for
@@ -124,3 +88,13 @@ model.fit(
         # at the end of each epoch
         validation_data=(x_eval, y_eval)
 )
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
